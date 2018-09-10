fe5e4071d77665a2f2e6e362988c23d3f674ee80
Andrew Lete <naliga24@gmail.com>
2018-09-10 20:48:07 +0700

    make it better

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def bot():
     # replyQueue.append(reponse_dict[closest])
    
     # ตอบข้อความ "นี่คือรูปแบบข้อความที่รับส่ง" กลับไป
-    replyQueue.append('นี่คือรูปแบบข้อความที่รับส่ง')
+    replyQueue.append('นี่คือรูปแบบข้อความที่รับส่งครับผม')
     
     # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไปมา (แบบ json)
     replyQueue.append(msg_in_string)
