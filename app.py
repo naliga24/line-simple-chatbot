35e1c7bc075a31fb06bf7fd4a33b28c6c50dbcec
Andrew Lete <naliga24@gmail.com>
2018-10-28 18:11:43 +0700

    from_shell

--- app.py
+++ app.py
@@ -1,53 +1,57 @@
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from flask import Flask, request
 import json
 import requests
-
+import database
 # ตรง YOURSECRETKEY ต้องนำมาใส่เองครับจะกล่าวถึงในขั้นตอนต่อๆ ไป
 global LINE_API_KEY
 # ห้ามลบคำว่า Bearer ออกนะครับเมื่อนำ access token มาใส่
 LINE_API_KEY = 'Bearer /mnxywYk+P8dLSFrPpEcZinPM5xmqGvzWGDLnOLhcmz3Iv4ymldO/P75wa3yPZCv2y4MNEMa/m9kHbaTHtKyxNJsoXIhWinqT8l94ePO7vflwsGHPiF0VzH8OSSL/4DRNH4zNVYWuvGDHAjyqPBuewdB04t89/1O/w1cDnyilFU='
 
 app = Flask(__name__)
-app.config['TESTING'] = True 
+app.config['TESTING'] = True
+
+
 @app.route('/')
 def index():
     return 'This is chatbot server!.'
-@app.route('/bot', methods=['POST'])
 
+
+@app.route('/bot', methods=['POST'])
 def bot():
     # ข้อความที่ต้องการส่งกลับ
     replyQueue = list()
-   
+
     # ข้อความที่ได้รับมา
     msg_in_json = request.get_json()
     msg_in_string = json.dumps(msg_in_json)
-    
+    print(msg_in_json)
     # Token สำหรับตอบกลับ (จำเป็นต้องใช้ในการตอบกลับ)
     replyToken = msg_in_json["events"][0]['replyToken']
-    
+
     # ส่วนนี้ดึงข้อมูลพื้นฐานออกมาจาก json (เผื่อ)
-    userID =  msg_in_json["events"][0]['source']['userId']
-    msgType =  msg_in_json["events"][0]['message']['type']
-    
+    userID = msg_in_json["events"][0]['source']['userId']
+    msgType = msg_in_json["events"][0]['message']['type']
+
     # ตรวจสอบว่า ที่ส่งเข้ามาเป็น text รึป่าว (อาจเป็น รูป, location อะไรแบบนี้ได้ครับ)
     # แต่ก็สามารถประมวลผลข้อมูลประเภทอื่นได้นะครับ
     # เช่น ถ้าส่งมาเป็น location ทำการดึง lat long ออกมาทำบางอย่าง เป็นต้น
     if msgType != 'text':
         reply(replyToken, ['Only text is allowed.'])
-        return 'OK',200
-    
-    # ตรงนี้ต้องแน่ใจว่า msgType เป็นประเภท text ถึงเรียกได้ครับ 
+        return 'OK', 200
+
+    # ตรงนี้ต้องแน่ใจว่า msgType เป็นประเภท text ถึงเรียกได้ครับ
     # lower เพื่อให้เป็นตัวพิมพ์เล็ก strip เพื่อนำช่องว่างหัวท้ายออก ครับ
     text = msg_in_json["events"][0]['message']['text'].lower().strip()
-    
+
     # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ exact match
     # response_dict = {'สวัสดี':'สวัสดีครับ'}
     # if text in response_dict:
     #     replyQueue.append(reponse_dict[text])
     # else:
     #     replyQueue.append('ไม่รู้ว่าจะตอบอะไรดี TT')
-       
+
     # ตัวอย่างการทำให้ bot ถาม-ตอบได้ แบบ non-exact match
     # โดยที่มี method ชื่อ find_closest_sentence ที่ใช้การเปรียบเทียบประโยค
     # เพื่อค้นหาประโยคที่ใกล้เคียงที่สุด อาจใช้เรื่องของ word embedding มาใช้งานได้ครับ
@@ -55,37 +59,39 @@ def bot():
     # response_dict = {'สวัสดี':'สวัสดีครับ'}
     # closest = find_closest_sentence(response_dict, text)
     # replyQueue.append(reponse_dict[closest])
-   
+
     # ตอบข้อความ "นี่คือรูปแบบข้อความที่รับส่ง" กลับไป
     replyQueue.append('นี่คือรูปแบบข้อความที่รับส่งครับ')
-    
+
     # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไปมา (แบบ json)
     replyQueue.append(msg_in_string)
     reply(replyToken, replyQueue[:5])
-    
+
     return 'OK', 200
- 
+
+
 def reply(replyToken, textList):
     # Method สำหรับตอบกลับข้อความประเภท text กลับครับ เขียนแบบนี้เลยก็ได้ครับ
-    LINE_API = 'https://api.line.me/v2/bot/message/reply'  #reply
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'  # reply
     headers = {
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization': LINE_API_KEY
     }
 
-    data ={
-    "replyToken":replyToken,
-    "messages":[{
-    "type": "location",
-    "title": "my location",
-    "address": "ฺBangkok, Thailand",
-    "latitude": 35.65910807942215,
-    "longitude": 139.70372892916203
-    }]
+    data = {
+        "replyToken": replyToken,
+        "messages": [{
+            "type": "location",
+            "title": "my location",
+            "address": "ฺBangkok, Thailand",
+            "latitude": 35.65910807942215,
+            "longitude": 139.70372892916203
+        }]
     }
-    
+
     requests.post(LINE_API, headers=headers, data=json.dumps(data))
     return
+
 
 if __name__ == '__main__':
     app.run()
---
+++ database.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import mysql.connector
+import cv
+import sys
+from PIL import Image
+import base64
+#import cStringIO
+import PIL.Image
+import io
+
+
+def select_class_attendace_info(studentId, subjectName):
+    mydb = mysql.connector.connect(
+        host="us-cdbr-iron-east-01.cleardb.net",
+        user="b2742dd9273833",
+        passwd="99f7887d5ff6a81",
+        database="heroku_766db354cb15187",
+    )
+    cursor = mydb.cursor()
+    
+    sql = "SELECT DATE_FORMAT( class_attendance_date,'%Y-%m-%d' ) , TIME_FORMAT( class_attendance_time,'%h:%m:%s' )"
+    sql += " FROM class_attendance_info"
+    sql += " WHERE student_id = '"+studentId+"'"
+    sql += " AND subject_id = (SELECT subject_id FROM subject_info WHERE subject_name = '"+subjectName+"')"
+    sql += " AND (SELECT semester_id FROM semester_info ORDER BY semester_id DESC LIMIT 1)"
+    sql += " ORDER BY class_attendance_date ASC , class_attendance_time ASC"
+
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    txt = ''
+    for row in result:
+        txt += row[0]+' , '+row[1]+'\n'
+
+    print(txt)
+    mydb.close()
+
+
+if __name__ == "__main__":
+    select_class_attendace_info('6005004780', 'cos1103')
