6e3a4be03d289d6f1f2146f771d9880ec98ff9d8
Andrew Lete <naliga24@gmail.com>
2019-02-13 18:55:29 +0700

    from_shell

--- dbConfig.py
+++ dbConfig.py
@@ -1,17 +1,17 @@
 import mysql.connector
 
-# def config():
-#     return mysql.connector.connect(
-#         host="us-cdbr-iron-east-01.cleardb.net",
-#         user="b2742dd9273833",
-#         passwd="99f7887d5ff6a81",
-#         database="heroku_766db354cb15187",
-#     )
-
 def config():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        passwd="root",
-        database="cos4105",
+        host="us-cdbr-iron-east-01.cleardb.net",
+        user="b2742dd9273833",
+        passwd="99f7887d5ff6a81",
+        database="heroku_766db354cb15187",
     )
+
+# def config():
+#     return mysql.connector.connect(
+#         host="127.0.0.1",
+#         user="root",
+#         passwd="root",
+#         database="cos4105",
+#     )
