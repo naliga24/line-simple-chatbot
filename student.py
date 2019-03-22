a0c30f5d6c245cec837576f475f51c24ccc45a78
Andrew Lete <naliga24@gmail.com>
2019-03-22 13:48:52 +0700

    from_shell

--- student.py
+++ student.py
@@ -13,10 +13,26 @@ def select_student_info(studentCodeName):
     result = cursor.fetchall()
     txt = ''
     for row in result:
-        txt += row[0]+' '+row[1]+' , '+row[2]+' '+row[3]+'\n'
+        txt += row[0]+' '+row[1]+' ('+row[2]+') '+row[3]+'\n'
+    mydb.close()
+    print('.'+txt)
+    return txt
+
+def select_student_info_all():
+    mydb = dbConfig.config()
+    cursor = mydb.cursor()
+    sql = "SELECT a.STUDENT_FIRST_NAME , a.STUDENT_LAST_NAME , b.USE_STATUS_DESCRIPTION , a.STUDENT_IMAGE"
+    sql += " FROM student_info a , use_status_info b"
+    sql += " WHERE a.STUDENT_STATUS = b.USE_STATUS_NO"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    txt = ''
+    for row in result:
+        txt += row[0]+' '+row[1]+' ('+row[2]+') '+row[3]+' ,\n'
     mydb.close()
     print('.'+txt)
     return txt
 
 if __name__ == "__main__":
-    select_student_info('6005004780')
+    #select_student_info('6005004780')
+    select_student_info_all()
